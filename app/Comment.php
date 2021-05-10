fab8b3914cb8ec37b6e902ad1fdc34df1f46ef92
klerik86elvin <62464631+klerik86elvin@users.noreply.github.com>
2021-05-10 14:57:16 +0400

    create emp attribute in Comment

--- app/Comment.php
+++ app/Comment.php
@@ -8,7 +8,8 @@ class Comment extends Model
 {
     protected $fillable = ['text', 'employee_id'];
 
-    protected $with = ['employee'];
+    protected $appends = ['emp'];
+
 
     public function task()
     {
@@ -18,5 +19,10 @@ class Comment extends Model
     public function employee()
     {
         return $this->belongsTo(Employee::class);
+    }
+
+    public function getEmpAttribute()
+    {
+        return $this->employee()->first();
     }
 }
