aafa9c02705d2f9107f3c8ed718700df7e9f42d4
klerik86elvin <62464631+klerik86elvin@users.noreply.github.com>
2021-05-11 16:22:45 +0400

    add employees relation in Department

--- app/Department.php
+++ app/Department.php
@@ -10,10 +10,16 @@ class Department extends Model
     use SoftDeletes;
 
     protected $fillable = ['name'];
-    protected $with = ['projects'];
+    protected $with = ['projects','employees'];
     protected $hidden = ['created_at', 'deleted_at', 'updated_at'];
     public function projects()
     {
         return $this->hasMany(Project::class);
+    }
+
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
     }
 }
